5669a8f4f61d65ab1acf8ec81c84758047f50421
John Malc <cincenko@outlook.com>
2017-03-25 20:09:08 +0100

    add bold text back

--- R/input-utils.R
+++ R/input-utils.R
@@ -1,5 +1,5 @@
 controlLabel <- function(controlName, label) {
-  label %AND% tags$label(class = "form-control-label", `for` = controlName, label)
+  label %AND% tags$label(class = "form-control-label font-weight-bold", `for` = controlName, label)
 }
 
 
